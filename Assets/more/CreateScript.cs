e5801a18ab936c4c05e2c43985e826806e9b80a4
jameslee5656 <james.lee56566@gmail.com>
2018-04-10 22:14:58 +0800

    Leap Motion(Assembly Line)

---
+++ Assets/more/CreateScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CreateScript : MonoBehaviour
+{
+    public GameObject Obj_Create;
+    private Transform tr;
+    private float spawnTime = 10f;
+    private float speed = 0.5f;
+    private GameObject clone;
+
+    // Use this for initialization
+    void Start()
+    {   
+        InvokeRepeating("launch", 2, 2.0f);
+    }
+
+    private void OnEnable()
+    {
+        tr = transform;
+        tr.position = new Vector3(-0.8f, 0.1f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.Translate(speed* Time.deltaTime, 0, 0);
+        spawnTime -= Time.deltaTime;
+        if (spawnTime < 0)
+        {
+            Destroy(clone);
+            if (Obj_Create)
+            {
+                Destroy(Obj_Create);
+      
+            }
+        }
+    }
+
+    void launch()
+    {        
+        if (!clone)
+        {
+            clone = Instantiate(Obj_Create);
+        }
+    }
+
+}
