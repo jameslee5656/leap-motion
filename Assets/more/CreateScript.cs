2732216cac678676e77334b86245f65991c10a27
jameslee5656 <james.lee56566@gmail.com>
2018-04-11 13:39:40 +0800

    with leap motion and correct the rotation and displacement problem

--- Assets/more/CreateScript.cs
+++ Assets/more/CreateScript.cs
@@ -3,36 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 public class CreateScript : MonoBehaviour
 {
+    private Vector3 normalizeDirection;
     public GameObject Obj_Create;
     private Transform tr;
     private float spawnTime = 10f;
     private float speed = 0.5f;
     private GameObject clone;
+    private float distance;
 
     // Use this for initialization
     void Start()
     {   
         InvokeRepeating("launch", 2, 2.0f);
+        normalizeDirection = new Vector3(1, 0, 0);
     }
 
     private void OnEnable()
     {
         tr = transform;
         tr.position = new Vector3(-0.8f, 0.1f, 0.5f);
+        tr.rotation = new Quaternion(0, 0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        tr.Translate(speed* Time.deltaTime, 0, 0);
+        //tr.Translate(speed* Time.deltaTime, 0, 0);
+        tr.position += normalizeDirection * speed * Time.deltaTime;
         spawnTime -= Time.deltaTime;
         if (spawnTime < 0)
         {
             Destroy(clone);
             if (Obj_Create)
             {
-                Destroy(Obj_Create);
-      
+                Destroy(Obj_Create);    
             }
         }
     }
